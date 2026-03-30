93aced096b56e0a8f64e0c70efc211228394e289
VDecimo <veridianopaulosch@gmail.com>
2026-03-30 11:00:25 +0800

    Navigation routes configuration, about section

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,10 +2,10 @@
     <div class="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         <h1 class="text-xl font-bold">Github</h1>
         <ul class="flex space-x-6">
-            <li><a href="#" class="hover:text-blue-500">Home</a></li>
-            <li><a href="#" class="hover:text-blue-500">About</a></li>
-            <li><a href="#" class="hover:text-blue-500">Contact</a></li>
-            <li><a href="#" class="hover:text-blue-500">Feature</a></li>
+            <li><a href="{{ route('home') }}" class="hover:text-blue-500">Home</a></li>
+            <li><a href="{{ route('about') }}" class="hover:text-blue-500">About</a></li>
+            <li><a href="{{ route('contact') }}" class="hover:text-blue-500">Contact</a></li>
+            <li><a href="{{ route('features') }}" class="hover:text-blue-500">Feature</a></li>
         </ul>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+//static pages
 Route::get('/about', [PageController::class, 'about'])->name('about');
-Route::get('/feature', [PageController::class, 'feature'])->name('feature');
+Route::get('/features', [PageController::class, 'feature'])->name('features');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+//dynamic pages
+Route::get('/about', [AboutController::class, 'index'])->name('about');
