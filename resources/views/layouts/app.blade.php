fe2e20071127537a971b0fb9bf99a9332142c422
Vaneric <vsanpascual.xoointern@gmail.com>
2026-03-30 14:35:09 +0800

    development hero nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,41 @@
             dot.style.transform = 'translateX(0)';
         }
     });
+
+    <script>
+const words = ["Web Development", "UI/UX Design", "System Solutions", "Modern Apps"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+    currentWord = words[i];
+    
+    if (isDeleting) {
+        j--;
+    } else {
+        j++;
+    }
+
+    document.getElementById("typed").textContent = currentWord.substring(0, j);
+
+    if (!isDeleting && j === currentWord.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % words.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
+</script>
 </script>
 </body>
 </html>
